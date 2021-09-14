c6bd0430dd6e98cd4e4773d0def2be9f4617a7b4
Yves Bode <ynrbode@gmail.com>
2021-09-14 10:47:29 -0300

    feat(todos): implements list method in the todos controller

--- src/modules/todos/http/controllers/todos-controller.ts
+++ src/modules/todos/http/controllers/todos-controller.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from 'express';
-import { getMongoRepository, getConnection } from 'typeorm';
+import { getConnection } from 'typeorm';
 
 import CreateTodoService from '@services/create-todo-service';
+import ListTodosService from '@services/list-todos-service';
 import Todo from '@entities/todo-entity';
 
 export default class TodosController {
   private createTodo: CreateTodoService;
 
+  private listTodos: ListTodosService;
+
   constructor() {
     this.createTodo = new CreateTodoService(
       getConnection().getMongoRepository(Todo),
     );
+
+    this.listTodos = new ListTodosService(
+      getConnection().getMongoRepository(Todo),
+    );
+  }
+
+  public async list(request: Request, response: Response): Promise<void> {
+    const { page } = request.query;
+    const list = await this.listTodos.execute({
+      page: page as unknown as number,
+    });
+
+    response.json(list);
   }
 
   public async create(request: Request, response: Response): Promise<void> {
