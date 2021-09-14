4593e0aa868a42e2b77c622958a5e095334b4987
Yves Bode <ynrbode@gmail.com>
2021-09-14 10:14:37 -0300

    feat(todos): implements validation in the todos route

--- src/modules/todos/http/routes/todos-route.ts
+++ src/modules/todos/http/routes/todos-route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import { celebrate, Segments } from 'celebrate';
 
 import TodosController from '@http/controllers/todos-controller';
+import createTodoRequestSchema from '@http/schemas/create-todo-request-schema';
 
 const todosRouter = Router();
 const todosController = new TodosController();
 
-todosRouter.post('/', todosController.create.bind(todosController));
+todosRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: createTodoRequestSchema,
+  }),
+  todosController.create.bind(todosController),
+);
 
 export default todosRouter;
