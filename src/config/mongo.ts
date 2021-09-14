4a6b0302fbb5ecb1e5aabf28e81f8120ea782a4a
Yves Bode <ynrbode@gmail.com>
2021-09-14 13:28:51 -0300

    feat(global): changes default value for the mongodb connection url

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,4 +1,4 @@
 export default {
-  url: process.env.MONGO_URL || 'mongodb://mongo:mongo@localhost:27017',
+  url: process.env.MONGO_URL || 'mongodb://localhost:27017',
   database: process.env.MONGO_DATABASE || 'catodo',
 };
