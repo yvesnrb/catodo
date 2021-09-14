315515b55a9bd01cddb8dd730f361a30d3a8b17c
Yves Bode <ynrbode@gmail.com>
2021-09-14 09:52:47 -0300

    feat(global): starts the server in the application entry point

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
 import 'dotenv/config';
 import 'reflect-metadata';
-import database from '@/db';
 
-database.then(() => {
+import database from '@/db';
+import serverConfig from '@config/server';
+
+database.then(async () => {
   console.log('MongoDB connected.');
+  const server = await import('@http/server');
+
+  server.default.listen(serverConfig.port, () => {
+    console.log(`Server listening on port ${serverConfig.port}.`);
+  });
 }).catch((reason) => {
   console.log('MongoDB connection failed.');
   console.log(reason);
