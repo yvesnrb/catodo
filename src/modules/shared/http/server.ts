c597f16c5d5d3865abc5480ace744e95779f8b47
Yves Bode <ynrbode@gmail.com>
2021-09-14 14:00:15 -0300

    docs(swagger): implements a render of the documentation under /documentation

--- src/modules/shared/http/server.ts
+++ src/modules/shared/http/server.ts
@@ -2,7 +2,9 @@ import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import swaggerUi from 'swagger-ui-express';
 
+import swaggerSpec from '@/swagger/spec.json';
 import handleError from '@http/middleware/handle-error-middleware';
 import routes from '@http/routes';
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errors());
 app.use(handleError);
