31a01d0b129764dd350bb174d95f2cc8d8445a66
Yves Bode <ynrbode@gmail.com>
2021-09-14 10:48:09 -0300

    feat(todos): implements GET route in the todos route

--- src/modules/todos/http/routes/todos-route.ts
+++ src/modules/todos/http/routes/todos-route.ts
@@ -3,15 +3,20 @@ import { celebrate, Segments } from 'celebrate';
 
 import TodosController from '@http/controllers/todos-controller';
 import createTodoRequestSchema from '@http/schemas/create-todo-request-schema';
+import listTodosQuerySchema from '@http/schemas/list-todos-query-schema';
 
 const todosRouter = Router();
 const todosController = new TodosController();
 
+todosRouter.get(
+  '/',
+  celebrate({ [Segments.QUERY]: listTodosQuerySchema }),
+  todosController.list.bind(todosController),
+);
+
 todosRouter.post(
   '/',
-  celebrate({
-    [Segments.BODY]: createTodoRequestSchema,
-  }),
+  celebrate({ [Segments.BODY]: createTodoRequestSchema }),
   todosController.create.bind(todosController),
 );
 
